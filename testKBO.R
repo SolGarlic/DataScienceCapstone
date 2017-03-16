b623cfbeaeac731d2db5a322a990abccd434b6fe
SolGarlic <cdascgil@gmail.com>
2017-03-16 23:33:51 +0000

    functions working: CalcProbALT (calculates the probability of a word after an ngram) CalcProbTermALT (calculates the most probable words)
    Needs some cleaning and optimization

--- testKBO.R
+++ testKBO.R
@@ -1,3 +1,31 @@
+CalcProbALT("The guy in front of me just bought a pound of bacon, a bouquet, and a case of", c("beer", "cheese", "pretzels", "soda"))
+CalcProbALT("You're the reason why I smile everyday. Can you follow me please? It would mean the", c("world", "most", "universe", "best"))
+CalcProbALT("Hey sunshine, can you follow me and make me the", c("happiest", "saddest", "bluest", "smelliest"))
+CalcProbALT("Very early observations on the Bills game: Offense still struggling but the", c("defense", "players", "crowd", "referees"))
+CalcProbALT("Go on a romantic date at the",c("beach","mall", "movies", "grocery"))
+CalcProbALT("Well I'm pretty sure my granny has some old bagpipes in her garage I'll dust them off and be on my", c("way", "motorcycle", "phone", "horse"))
+CalcProbALT("Ohhhhh #PointBreak is on tomorrow. Love that film and haven't seen it in quite some", c("time", "weeks", "years", "thing"))
+CalcProbALT("After the ice bucket challenge Louis will push his long wet hair out of his eyes with his little", c("fingers", "eyes", "ears", "toes"))
+CalcProbALT("Be grateful for the good times and keep the faith during the", c("bad", "sad", "worst", "hard"))
+CalcProbALT("If this isn't the cutest thing you've ever seen, then you must be", c("insane", "callous", "insensitive", "asleep"))
+
+CalcProbALT("When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd", c("die", "eat", "sleep", "give"))
+CalcProbALT("Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his", c("marital", "spiritual", "financial", "horticultural"))
+CalcProbALT("I'd give anything to see arctic monkeys this", c("weekend", "month", "morning", "decade"))
+CalcProbALT("Talking to your mom has the same effect as a hug and helps reduce your", c("stress", "sleepiness", "hunger", "happiness"))
+CalcProbALT("When you were in Holland you were like 1 inch away from me but you hadn't time to take a", c("walk", "look", "picture", "minute"))
+CalcProbALT("I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the", c("incident", "matter", "account", "case"))
+CalcProbALT("I can't deal with unsymetrical things. I can't even hold an uneven number of bags of groceries in each", c("hand", "arm", "finger", "toe"))
+CalcProbALT("Every inch of you is perfect from the bottom to the", c("top", "side", "center", "middle"))
+CalcProbALT("Im thankful my childhood was filled with imagination and bruises from playing", c("outside", "inside", "daily", "weekly"))
+CalcProbALT("I like how the same people are in almost all of Adam Sandler's", c("movies", "novels", "stories", "pictures"))
+
+
+## OBSOLETE BELOW!!!      
+      
+
+
+
 c<-list(1:10)
 c[[1]]<-rbind(
       CalcProb("The guy in front of me just bought a pound of bacon, a bouquet, and a case of beer")
--- tokenizeInt.R
+++ tokenizeInt.R
@@ -291,9 +291,115 @@ ReverseIndexLevels <- function(df, v=vocab) {
       foo
 }
 
-CalcProbTerms <- function(txt) {
-      # returns the most probable words (and their probability) to appear after txt variable
+CalcProbTermsALT <- function(txt) {
+      
+      # create indexed and clean 3gram with txt words
+      library(tm)
+      # clean the text with the same procedure as the training corpus
+      # and return the vector with the ngram
+      
+      txt<-removePunctuation(txt)
+      txt<-removeNumbers(txt)
+      txt<-tolower(txt)
+      txt<-removeWords(txt, tm::stopwords("en"))
+      txt<-stemDocument(txt)
+      ntxt<- unlist(strsplit(txt," +"))
+      
+      # source <- VectorSource(txt)
+      # PC2 <- VCorpus(source,
+      #                readerControl = list(reader = readPlain,
+      #                                     language = "en", load = TRUE))
+      # PC2 <- cleanCorpus(PC2)
+      # ntxt<- unlist(strsplit(PC2[[1]][[1]][1]," +"))
 
+      #      ntxt<- unlist(strsplit(txt," "))
+      
+      n <- length(ntxt)
+      ntxt<- tail(ntxt,3) # assumes n>=3
+      txt<-match(ntxt,vocab)
+      # if 3gram exists in DB, find most common words
+      # find group of 4grams with first 3grams == txt
+
+      group4<-C4[.(txt[1],txt[2],txt[3])]
+      if(sum(is.na(group4$freq)==0)) {
+            words4<-head(group4[,.(X=X4, prob=cGT/sum(freq))][order(-prob)],10)
+            
+            beta<-C3[.(txt[1],txt[2],txt[3])]$leftover
+            
+            #next, even if 3gram is found, calc prob of words after only 2gram
+            group3<-C3[.(txt[2],txt[3])][!(X3 %in% words4$X)]
+            alpha<-beta/(sum(group3$cGT)/sum(group3$freq))
+            words3<-head(group3[,.(X=X3, prob=alpha*cGT/sum(freq))][order(-prob)],10)
+            
+            #next, even if 2gram is found, calc prob of words after only 1gram
+            group2<-C2[.(txt[3])][!(X2 %in% c(words4$X,words3$X))]
+            alpha<-beta/(sum(group2$cGT)/sum(group2$freq))
+            words2<-head(group2[,.(X=X2, prob=alpha*cGT/sum(freq))][order(-prob)],10)
+            
+            #next, also calc prob of single words (really not needed, but)
+            group1<-C1[!(X1 %in% c(words4$X,words3$X,words2$X))]
+            alpha<-beta/(sum(group1$cGT)/sum(group1$freq))
+            words1<-head(group1[,.(X=X1, prob=alpha*cGT/sum(freq))][order(-prob)],10)
+            
+            words<- rbind(words4, words3, words2, words1)
+            words<-words[,.(X=vocab[X],prob)][order(-prob)]
+            words<-stemCompletion(words, StemDictionary$token, type="first")
+            return(words)
+      }
+      
+      group3<-C3[.(txt[2],txt[3])]
+      if(sum(is.na(group3$freq)==0)) {
+            words3<-head(group3[,.(X=X3, prob=cGT/sum(freq))][order(-prob)],10)
+            
+            beta<-C2[.(txt[2],txt[3])]$leftover
+            
+            #next, even if 2gram is found, calc prob of words after only 1gram
+            group2<-C2[.(txt[3])][!(X2 %in% c(words3$X))]
+            alpha<-beta/(sum(group2$cGT)/sum(group2$freq))
+            words2<-head(group2[,.(X=X2, prob=alpha*cGT/sum(freq))][order(-prob)],10)
+            
+            #next, also calc prob of single words (really not needed, but)
+            group1<-C1[!(X1 %in% c(words3$X,words2$X))]
+            alpha<-beta/(sum(group1$cGT)/sum(group1$freq))
+            words1<-head(group1[,.(X=X1, prob=alpha*cGT/sum(freq))][order(-prob)],10)
+            
+            words<- rbind(words3, words2, words1)
+            words<-words[,.(X=vocab[X],prob)][order(-prob)]
+            words<-stemCompletion(words, StemDictionary$token, type="first")
+            return(words)
+      }
+
+      group2<-C2[.(txt[3])][order(-cGT)]
+      if(sum(is.na(group2$freq)==0)) {
+            words2<-head(group2[,.(X=X2, prob=cGT/sum(freq))][order(-prob)],10)
+            
+            beta<-C1[.(txt[3])]$leftover
+            
+            #next, also calc prob of single words (really not needed, but)
+            group1<-C1[!(X1 %in% c(words2$X))]
+            alpha<-beta/(sum(group1$cGT)/sum(group1$freq))
+            words1<-head(group1[,.(X=X1, prob=alpha*cGT/sum(freq))][order(-prob)],10)
+            
+            words<- rbind(words2, words1)
+            words<-words[,.(X=vocab[X],prob)][order(-prob)]
+            words<-stemCompletion(words, StemDictionary$token, type="first")
+            return(words)
+      }
+
+      group1<-C1
+      if(sum(is.na(group1$freq)==0)) {
+            words1<-head(group1[,.(X=X1, prob=cGT/sum(freq))][order(-prob)],10)
+            
+            words<-words[,.(X=vocab[X],prob)][order(-prob)]
+            words<-stemCompletion(words, StemDictionary$token, type="first")
+            return(words)
+      }
+}
+
+
+CalcProbALT <- function(txt="white house officials",words=c("said", "spoke", "told", "announced")) {
+      
+      # create indexed and clean 3gram with txt words
       library(tm)
       # clean the text with the same procedure as the training corpus
       # and return the vector with the ngram
@@ -306,48 +412,101 @@ CalcProbTerms <- function(txt) {
       #      ntxt<- unlist(strsplit(txt," "))
       n <- length(ntxt)
       ntxt<- tail(ntxt,3) # assumes n>=3
-      index<-match(ntxt,vocab)
-
-      # Check if this ngram exists in C4:
-      subset <- C4[.(index[1], index[2], index[3])]
-      if(!is.na(sum(subset$freq))) {  #if subset exists, calculates the probabilities
-            # first, of existing 
-            z<-subset[,Prob:=cGT/sum(freq)][,.(X4, Prob)][order(-Prob)]
-            return(head(z,10))
+      txt<-match(ntxt,vocab)
+      
+      #convert the words to indexes
+      words<-stemDocument(words)
+      words<-match(words,vocab)
+      
+      # if 3gram exists in DB, find most common words
+      # find group of 4grams with first 3grams == txt
+      
+      group4<-C4[.(txt[1],txt[2],txt[3])]
+      if(sum(is.na(group4$freq)==0)) {
+            words4<-group4[X4 %in% words,.(X=X4, prob=cGT/sum(freq))][order(-prob)]
             
-            # second, of 
+            beta<-C3[.(txt[1],txt[2],txt[3])]$leftover
+            words<-words[!(words %in% words4$X)]
+            #next, even if 3gram is found, calc prob of words after only 2gram
+            group3<-C3[.(txt[2],txt[3])][!(X3 %in% words4$X)]
+            alpha<-beta/(sum(group3$cGT)/sum(group3$freq))
+            words3<-group3[X3 %in% words,.(X=X3, prob=alpha*cGT/sum(freq))][order(-prob)]
+            words<-words[!(words %in% words3$X)]
             
-      }
-
-      subset <- C3[.(index[2], index[3])]
-      if(!is.na(sum(subset$freq))) {  #if subset exists, calculates the probabilities
-            # first, of existing 
-            z<-subset[,Prob:=cGT/sum(freq)][,.(X3, Prob)][order(-Prob)]
-            return(head(z,10))
-      }
-
-      subset <- C2[.(index[3])]
-      if(!is.na(sum(subset$freq))) {  #if subset exists, calculates the probabilities
-            # first, of existing 
-            z<-subset[,Prob:=cGT/sum(freq)][,.(X2, Prob)][order(-Prob)]
-            return(head(z,10))
-      }
-
-      subset <- C1
-      if(!is.na(sum(subset$freq))) {  #if subset exists, calculates the probabilities
-            # first, of existing 
-            z<-subset[,Prob:=cGT/sum(freq)][,.(X1, Prob)][order(-Prob)]
-            return(head(z,10))
+            #next, even if 2gram is found, calc prob of words after only 1gram
+            group2<-C2[.(txt[3])][!(X2 %in% c(words4$X,words3$X))]
+            alpha<-beta/(sum(group2$cGT)/sum(group2$freq))
+            words2<-group2[X2 %in% words,.(X=X2, prob=alpha*cGT/sum(freq))][order(-prob)]
+            words<-words[!(words %in% words2$X)]
+            
+            #next, also calc prob of single words (really not needed, but)
+            group1<-C1[!(X1 %in% c(words4$X,words3$X,words2$X))]
+            alpha<-beta/(sum(group1$cGT)/sum(group1$freq))
+            words1<-group1[X1 %in% words,.(X=X1, prob=alpha*cGT/sum(freq))][order(-prob)]
+            
+            words<- rbind(words4, words3, words2, words1)
+            words<-words[,.(X=vocab[X],prob)][order(-prob)]
+            return(words)
       }
       
+      group3<-C3[.(txt[2],txt[3])]
+      if(sum(is.na(group3$freq)==0)) {
+            words3<-group3[X3 %in% words,.(X=X3, prob=cGT/sum(freq))][order(-prob)]
+            words<-words[!(words %in% words3$X)]
+            
+            beta<-C2[.(txt[2],txt[3])]$leftover
+            
+            #next, even if 2gram is found, calc prob of words after only 1gram
+            group2<-C2[.(txt[3])][!(X2 %in% c(words3$X))]
+            alpha<-beta/(sum(group2$cGT)/sum(group2$freq))
+            words2<-group2[X2 %in% words,.(X=X2, prob=alpha*cGT/sum(freq))][order(-prob)]
+            words<-words[!(words %in% words2$X)]
+            
+            #next, also calc prob of single words (really not needed, but)
+            group1<-C1[!(X1 %in% c(words3$X,words2$X))]
+            alpha<-beta/(sum(group1$cGT)/sum(group1$freq))
+            words1<-group1[X1 %in% words,.(X=X1, prob=alpha*cGT/sum(freq))][order(-prob)]
+            
+            words<- rbind(words3, words2, words1)
+            words<-words[,.(X=vocab[X],prob)][order(-prob)]
+            return(words)
+      }
+      
+      group2<-C2[.(txt[3])]
+      if(sum(is.na(group2$freq)==0)) {
+            words2<-group2[X2 %in% words,.(X=X2, prob=cGT/sum(freq))][order(-prob)]
+            words<-words[!(words %in% words2$X)]
+            
+            beta<-C1[.(txt[3])]$leftover
+            
+            #next, also calc prob of single words (really not needed, but)
+            group1<-C1[!(X1 %in% c(words2$X))]
+            alpha<-beta/(sum(group1$cGT)/sum(group1$freq))
+            words1<-group1[X1 %in% words,.(X=X1, prob=alpha*cGT/sum(freq))][order(-prob)]
+            
+            words<- rbind(words2, words1)
+            words<-words[,.(X=vocab[X],prob)][order(-prob)]
+            return(words)
+      }
+      
+      group1<-C1
+      if(sum(is.na(group1$freq)==0)) {
+            words1<-group1[X1 %in% words,.(X=X1, prob=alpha*cGT/sum(freq))][order(-prob)]
+            
+            words<-words[,.(X=vocab[X],prob)][order(-prob)]
+            return(words)
+      }
 }
-      
+
+
+
 
 CalcProb <- function(txt) {
       # returns the probability of the last word in the txt variable
       library(tm)
       # clean the text with the same procedure as the training corpus
       # and return the vector with the ngram
+      # this version uses a CC table in which the first column contains all txt ngrams 
       source <- VectorSource(txt)
       PC2 <- VCorpus(source,
                      readerControl = list(reader = readPlain,
